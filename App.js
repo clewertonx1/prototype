5823873f958da81bb8c7567684d06ad1f7464a09
Francisco Clewerton Pereira Roque <francisco.pereira@normatel.local>
2021-01-28 10:25:28 -0300

    teste

--- App.js
+++ App.js
@@ -1,12 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import React, {useEffect, useState} from 'react';
+import { StyleSheet, Text, View, Button} from 'react-native';
+import socket from "socket.io-client"
+
 
 export default function App() {
+  const url = "http://10.100.98.138:4000/"
+  const io = socket(url)
+
+  const [teste, setTeste] = useState(0)
+
+  useEffect(() => {
+    const io = socket(url)
+    console.log(teste)
+   
+    io.emit("a", {a: "cabeça de htttp"});
+  },[teste])
+
+  io.on("brabo", (req) =>{
+    console.log(req)
+  })
+
+ 
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{teste}</Text>
+      <Button title={"aaa"} onPress={() => {
+         let a = teste + 1 
+         setTeste(a)
+      }}></Button>
       <StatusBar style="auto" />
+
     </View>
   );
 }
