b0a7463ed01b080eb850edc6088a776bb0502abd
Francisco Clewerton Pereira Roque <francisco.pereira@normatel.local>
2021-01-28 12:12:20 -0300

    notify

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 
 import React, {useEffect, useState} from 'react';
-import { StyleSheet, Text, View, Button} from 'react-native';
+import { StyleSheet, Text, View, Button, Modal, TouchableHighlight, TouchableOpacity} from 'react-native';
 import socket from "socket.io-client"
 
 
@@ -9,28 +9,70 @@ export default function App() {
   const url = "http://10.100.98.138:4000/"
   const io = socket(url)
 
-  const [teste, setTeste] = useState(0)
 
-  useEffect(() => {
-    const io = socket(url)
-    console.log(teste)
-   
-    io.emit("a", {a: "cabeça de htttp"});
-  },[teste])
+  const [modalVisible, setModalVisible] = useState(false)
+  const [title, setTitle] = useState("")
+  const [id, setId] = useState()
 
-  io.on("brabo", (req) =>{
+  const sendRes = (res) =>{
+    console.log("teste", res)
+    io.emit("resNotify", res)
+  }
+
+  io.on("Accepted", (req) => {
+
+  })
+
+  io.on("notify", (req) =>{
+    console.log("notificaçao")
     console.log(req)
+    setTitle(req.title)
+    setId(req.id)
+    console.log(req.title, req.id)
+    setModalVisible(true)
   })
 
  
 
   return (
     <View style={styles.container}>
-      <Text>{teste}</Text>
-      <Button title={"aaa"} onPress={() => {
-         let a = teste + 1 
-         setTeste(a)
-      }}></Button>
+      <Text>TESTE</Text>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>{title}</Text>
+            <Text style={{...styles.modalText, fontSize: 15,}}>{id}</Text>
+            <View style={{flexDirection: 'row'}}>
+              <TouchableOpacity
+                style={{...styles.button, borderColor: "#05C46B"}}
+                onPress={() => {
+                  sendRes(true)
+                  setModalVisible(!modalVisible);
+                }}
+              >
+                <Text style={{...styles.textStyle,color: "#05C46B"}}>Aceitar</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={{...styles.button, borderColor: "#FF3F34"}}
+                onPress={() => {
+                  sendRes(false)
+                  setModalVisible(!modalVisible);
+                }}
+              >
+                <Text style={{...styles.textStyle,color: "#FF3F34"}}>Rejeitar</Text>
+              </TouchableOpacity>
+            </View>
+            
+          </View>
+        </View>
+      </Modal>
       <StatusBar style="auto" />
 
     </View>
@@ -38,10 +80,48 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  container: {
+  centeredView: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
   },
+  button:{
+    flex:1 ,
+    height: 50,
+    borderRadius: 15,
+    margin:5, 
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: 'transparent',
+    borderWidth: 3,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5
+  },
+  textStyle: {
+    fontSize: 20,
+
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  modalText: {
+    fontSize: 30,
+    color: '#2F2D2D',
+    marginBottom: 15,
+    textAlign: "center"
+  }
 });
+
